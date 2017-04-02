866ea02f36e0bf082e4e010b51795009dbe56afd
jeffnjut <jeff_njut@163.com>
2017-04-02 21:21:10 +0800

    add FJRefresh Header

---
+++ FJRefresh/Classes/FJRefresh.h
@@ -0,0 +1,17 @@
+//
+//  FJRefresh.h
+//  FJRefresh
+//
+//  Created by Jeff on 2017/4/2.
+//  Copyright © 2017年 Jeff. All rights reserved.
+//
+
+#ifndef FJRefresh_h
+#define FJRefresh_h
+
+#import <FJRefresh/PeapotRefreshAutoFooter.h>
+#import <FJRefresh/PeapotRefreshBackFooter.h>
+#import <FJRefresh/PeapotRefreshHeader.h>
+
+
+#endif /* FJRefresh_h */
